64b6bb7d84d2f951997cf51599b065f8b54e91b0
tiziicontreras <tizianob08@gmail.com>
2025-10-07 16:49:50 -0300

    repaso de media querys; creacion de primera interfaz y uso de directiva ngForpara tarjetas

---
+++ src/app/models/tarjeta.ts
@@ -0,0 +1,8 @@
+export interface Tarjeta {
+    id: string; //numeros como letras -> ej : 123AB / 123 / ABC
+    img: string;
+    alt: string;
+    titulo: string;
+    descripcion: string;
+    fechaPublicacion: number;
+}
--- src/app/pages/product/product.ts
+++ src/app/pages/product/product.ts
@@ -1,11 +1,62 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Tarjeta } from '../../models/tarjeta';
+
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.html',
   styleUrl: './product.css'
 })
 export class Product {
+  /*
+  Propiedad PUBLICA: Libre acceso a su contenido
+    Propiedad PRIVADA: Contenido restringido
+  */
+  public infoTarjera: Tarjeta[];
 
+  /* 
+  Declaramos coleccion de tarjetas en base a modelo de interfaz "Tarjeta"
+  - Se tiene que respetar el uso de todas las propiedades y su tipo
+  - Tipo alfanumerico (string) inicializa en ""
+  - Tipo numerico (number) inicializa en 0
+  */
+
+  constructor(){
+    this.infoTarjera = [
+      {
+        id: "1T",
+        img: "../../../assets/gettyimages-2222589873-612x612.jpg",
+        alt: "foto",
+        titulo: "Tienda compa",
+        descripcion: "flama",
+        fechaPublicacion: 2025
+      },
+      {
+       id: "2T",
+        img: "../../../assets/71HxqwN70xL.jpg",
+        alt: "foto",
+        titulo: "Tienda compa",
+        descripcion: "flama",
+        fechaPublicacion: 2025 
+      },
+      {
+        id: "3T",
+        img: "../../../assets/gettyimages-2222589873-612x612.jpg",
+        alt: "foto",
+        titulo: "Tienda compa",
+        descripcion: "flama",
+        fechaPublicacion: 2025
+      },
+      {
+       id: "4T",
+        img: "../../../assets/71HxqwN70xL.jpg",
+        alt: "foto",
+        titulo: "Tienda compa",
+        descripcion: "flama",
+        fechaPublicacion: 2025 
+      }
+    ]
+  }
 }
